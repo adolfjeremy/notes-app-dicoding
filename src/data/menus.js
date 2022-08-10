7c65b02ab99c762e727075169e4f1e45cc7264a8
adolfjeremy <adolf.jer@gmail.com>
2022-08-10 19:11:27 +0700

    fix image url

--- src/data/menus.js
+++ src/data/menus.js
@@ -2,20 +2,20 @@ const menus = {
     addNote: {
         title: "Add Note",
         control: "add-form",
-        img: "/icons/add.png",
+        img: "icons/add.png",
     },
     navList: [
         {
             id: 1,
             title: "Active Notes",
             control: "active-notes",
-            img: "/icons/note.png",
+            img: "icons/note.png",
         },
         {
             id: 2,
             title: "Archive Notes",
             control: "archive-notes",
-            img: "/icons/archive.png",
+            img: "icons/archive.png",
         },
     ],
 };
